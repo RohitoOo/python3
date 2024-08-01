ca71d9e2a7683d9fe01ca23aad28637feaa1bbcf
RohitoOo <bhambhani.ba@gmail.com>
2024-07-31 22:37:41 -0400

    July31st

--- python_sandbox_starter/lists.py
+++ python_sandbox_starter/lists.py
@@ -1,1 +1,24 @@
 # A List is a collection which is ordered and changeable. Allows duplicate members.
+
+# Create a list 
+
+numbers = [1,2,3,4,5]
+fruits = ['A', "B", "C", "D"]
+
+
+# Constructor
+numbers2 = list((1,2,3,4,0))
+print (numbers, numbers)
+
+# Get a value  
+print(fruits[0])
+
+fruits.append('M')
+fruits.insert(2,'L')
+
+print(fruits)
+
+fruits.pop(3)
+fruits.sort(reverse=True)
+
+print(fruits)
